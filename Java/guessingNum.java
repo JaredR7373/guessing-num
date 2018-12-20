373636e95bce54065a389c950169da6c9ed71eac
JaredR7373 <nubismoaf@gmail.com>
2018-12-20 18:32:57 -0500

    Updated guessingNum.java

---
+++ Java/guessingNum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class guessingNum
+{
+	public static void main(String[] args)
+	{
+		int max, min, diff;
+		
+		Random rand = new Random();
+		Scanner key = new Scanner(System.in);
+				
+		System.out.print("Enter a difficulty [1-5] ");
+		diff = key.nextInt();
+	}
+}
+		
